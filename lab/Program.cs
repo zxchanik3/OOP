10c604130d6556f616110a238b1ce39fba91b551
Андрій Цуз <andr45677@gmail.com>
2025-10-30 10:23:42 +0200

    replace code class track in another file

--- lab/Program.cs
+++ lab/Program.cs
@@ -1,19 +1,3 @@
 ﻿using System.Collections.Generic;
 
-public class Track
-{
-    public string Name { get; set; }
-    public int RequiredLapCount { get; set; }
-    public double TotalLength { get; set; }
 
-    public double StartLineX { get; set; }
-    public double StartLineY { get; set; }
-    public Track(string name, int lapCount, double length, double startX, double startY)
-    {
-        Name = name;
-        RequiredLapCount = lapCount;
-        TotalLength = length;
-        StartLineX = startX;
-        StartLineY = startY;
-    }
-}
