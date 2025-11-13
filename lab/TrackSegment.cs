3f3d033f38e33dc0f0010d17989d1950160cf91b
Андрій Цуз <andr45677@gmail.com>
2025-11-13 17:11:27 +0200

    add segment type, upgrade track, tracksegment

--- lab/TrackSegment.cs
+++ lab/TrackSegment.cs
@@ -8,10 +8,30 @@ namespace lab
 {
     public class TrackSegment
     {
-        public double StrartX {  get; set; }
-        public double StrartY { get; set; }
         public double Length { get; set; }
-      //  public SegmentType Type { get; set; }
+        public SegmentType Type { get; set; }
+        
+        // 1.0 для Straight, 0.5 для Corner, 0.0 для PitLane (тоді Car лише гальмує до MinSpeedLimit)
+        public double AccelerationFactor { get; set; }
+        
+        // якщо це поворот, машина гальмує до цього значення; 
+        // якщо це PitLane, вона не може перевищувати це значення.
+        public double MinSpeedLimit { get; set; } 
+        
+        public float WearFactor { get; set; } // наскільки сильно зношуються шини на цій ділянці, типу на поворотах вище
+
+        public TrackSegment(SegmentType type, double length, double accelFactor, double minSpeedLimit, float wearFactor)
+        {
+            Type = type;
+            Length = length;
+            AccelerationFactor = accelFactor;
+            MinSpeedLimit = minSpeedLimit;
+            WearFactor = wearFactor;
+        }
+        ~TrackSegment()
+        {
+            Console.WriteLine($"Об'єкт TrackSegment ({Type}) знищено.");
+        }
         
     }
 }
